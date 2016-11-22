6b28fafaf2459e58e309cc85bba27d4a37baac58
Alma Maizels <aymaiz@yahoo.com>
2016-11-21 19:14:24 -0800

    reqs done

--- noise.js
+++ noise.js
@@ -1,40 +1,61 @@
-// We'll be binding two different kinds of interaction to the music boxes.
-// Our initial goal will to be trigger the appropriate tone when someone clicks inside a box.
-// Once that is working we will extend the interaction to be triggered when someone presses a key on the keyboard.
+// @TODO Did not get to clearing the latest noteId so if an invalid note is typed the last note played will be played again
+
 
 $(document).ready( function() {
-  // $('.note').click(function(event) {
-  $('.note').on('click', function(event) {
+  $('body').on('keydown', function(event) {
+    var noteId = selectNote(event.key);
+    return doReMi(noteId);
+  });
 
-    console.log(event.which); //mouse click is '1'
-
-    switch($(this).html()) {
-      case 'c':
-        noteId = '#cAudio';
-        break;
-      case 'd':
-        noteId = '#dAudio';
-        break;
-      case 'e':
-        noteId = '#eAudio';
-        break;
-      case 'f':
-        noteId = '#fAudio';
-        break;
-      case 'g':
-        noteId = '#gAudio';
-        break;
-      case 'a':
-        noteId = '#aAudio';
-        break;
-      case 'b':
-        noteId = '#bAudio';
-        break;
-      default:
-        break;
-    }
-    $(noteId)[0].load();
-    $(noteId)[0].play();
-
+  $('.note').on('click', function() {
+    var noteId = selectNote($(this).html());
+    return doReMi(noteId);
   });
 });
+
+var doReMi = function(input) {
+  var noteId = input;
+  $(noteId)[0].load();
+  $(noteId)[0].play();
+  // noteId = null;
+};
+
+var selectNote = function(note) {
+  return '#' + note + 'Audio';
+};
+
+
+// var selectNote = function(note) {
+//   switch(note) {
+//     case 'c':
+//       return noteId = '#cAudio';
+//     case 'd':
+//       return noteId = '#dAudio';
+//     case 'e':
+//       return noteId = '#eAudio';
+//     case 'f':
+//       return noteId = '#fAudio';
+//     case 'g':
+//       return noteId = '#gAudio';
+//     case 'a':
+//       return noteId = '#aAudio';
+//     case 'b':
+//       return noteId = '#bAudio';
+//     default:
+//       // noteId = undefined;
+//       break;
+//   }
+// };
+
+// var selectNote = function(note) {
+//   var noteIds = {
+//     'c': '#cAudio',
+//     'd': '#dAudio',
+//     'e': '#eAudio',
+//     'f': '#fAudio',
+//     'g': '#gAudio',
+//     'a': '#aAudio',
+//     'b': '#bAudio'
+//   };
+//   return noteIds[note];
+// };
